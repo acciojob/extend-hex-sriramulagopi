ea02fd7160422d850b22db0bb3a40b3d9eb06fce
sriramulagopi <129825285+sriramulagopi@users.noreply.github.com>
2023-12-12 14:05:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 const extendHex = (shortHex) => {
-  // write your code here
+    // write your code here
+let str ="#";
+let count = 0;
+for (let i=1;i<shortHex.length;i++){
+    if(shortHex[i]>='A' && shortHex[i]<='Z'){
+        count++;
+    }
+    str+=shortHex[i]+shortHex[i];
+    
+}
+if(count>0){
+    return str.toUpperCase();
+}
+return str.toLowerCase();
 };
 
 // Do not change the code below.
