5c9042b634e6db0b03ba85adb88fe48907c592c7
sriramulagopi <129825285+sriramulagopi@users.noreply.github.com>
2024-01-31 16:07:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,29 @@
 const extendHex = (shortHex) => {
     // write your code here
-let str ="#";
+let str =["#"];
 let count = 0;
-for (let i=1;i<shortHex.length;i++){
-    if(shortHex[i]>='A' && shortHex[i]<='Z'){
-        count++;
-    }
-    str+=shortHex[i]+shortHex[i];
-    
-}
-if(count>0){
-    return str.toUpperCase();
-}
-return str.toLowerCase();
-};
+	if(shortHex.length>3){
+		for (let i=1;i<shortHex.length;i++){
+			if(shortHex[i]>='A' && shortHex[i]<='Z'){
+				count++;
+			}
+			str.push(shortHex[i]+shortHex[i]);
+		}
+	}
+	else{
+		for (let i=0;i<shortHex.length;i++){
+		    if(shortHex[i]>='A' && shortHex[i]<='Z'){
+		        count++;
+		    }
+		    str.push(shortHex[i]+shortHex[i]);
+		}
+	}
 
+if(count>0){
+    return str.join("").toUpperCase();
+}
+return str.join("").toLowerCase();
+};
 // Do not change the code below.
 const shortHex = prompt("Enter Short Hex.");
 alert(extendHex(shortHex));
